b11f65618c30d82b8ae50e46bf467be449cd27f3
Gulfaraz Yasin <gulfarazyasin@gmail.com>
2014-10-15 01:30:46 +0530

    Improved AI

--- js/main.js
+++ js/main.js
@@ -23,7 +23,8 @@ var user_interface = {
     ai_check: null,
     ai_player: 1,
     ai_delay: 100,
-    is_ai: true
+    is_ai: true,
+    grid_size: 3
 };
 
 function start_game() {
@@ -43,19 +44,19 @@ function start_game() {
     user_interface.score1element = document.getElementById("score1");
     user_interface.score2element = document.getElementById("score2");
     user_interface.ai_check = document.getElementById("ai_check");
-    
+
 
     /* Assign Functionality to play */
     add_listener(user_interface.cells, 'click', move);
     add_listener([user_interface.new_game], 'click', new_game);
     add_listener([user_interface.reset_scores], 'click', reset_scores);
     add_listener([user_interface.ai_check], 'change', check_ai);
-    
+
     /* Set Params */
-    if(!user_interface.is_ai) {
+    if (!user_interface.is_ai) {
         user_interface.ai_check.checked = undefined;
     } else {
-        if(user_interface.ai_player == 0) {
+        if (user_interface.ai_player == 0) {
             ai_move();
         }
     }
@@ -65,7 +66,7 @@ function start_game() {
 }
 
 function generate_game_area() {
-    var gameHTML = "<div id=\"options\"><input type=\"checkbox\" id=\"ai_check\" checked=\"checked\" /><label for=\"ai\">Player 2 is Computer</label></div><div class=\"instance\">";
+    var gameHTML = "<div id=\"options\"><input type=\"checkbox\" id=\"ai_check\" checked=\"checked\" /><label for=\"ai\">Computer Opponent</label></div><div class=\"instance\">";
     for (var i = 1; i < 10; i++) {
         var filler = "?";
         if (user_interface.debug) {
@@ -145,8 +146,8 @@ function move() {
                 } else {
                     user_interface.player++;
                     user_interface.player %= 2;
-                    if(user_interface.ai_check.checked && ((user_interface.moves % 2) == user_interface.ai_player)) {
-                        setTimeout(ai_move , user_interface.ai_delay);
+                    if (user_interface.ai_check.checked && ((user_interface.moves % 2) == user_interface.ai_player)) {
+                        setTimeout(ai_move, user_interface.ai_delay);
                     }
                 }
             }
@@ -160,21 +161,124 @@ function check_ai() {
 
 function ai_move() {
     var ai_options = [];
-    for(var i=0; i<user_interface.cells.length; i++) {
+    for (var i = 0; i < user_interface.cells.length; i++) {
         var value = user_interface.cells[i].innerHTML;
         if (value != "X" && value != "O") {
             ai_options.push(i);
         }
     }
-    user_interface.cells[ai_options[pick_move(ai_options)]].click();
+    user_interface.cells[pick_move(ai_options)].click();
 }
 
 function pick_move(ai_options) {
-    return Math.floor(Math.random() * ai_options.length);
+    var cell_weights = {};
+    for (var i = 0; i < ai_options.length; i++) {
+        var current_cell = ai_options[i];
+        var cell_groups = get_cell_groups(ai_options[i]);
+        for (var j = 0; j < cell_groups.length; j++) {
+            if (!cell_weights[current_cell]) {
+                cell_weights[current_cell] = [];
+            }
+            cell_weights[current_cell].push(cell_group_rank(cell_groups[j]));
+        }
+    }
+    var best_cell_group = best_cells(rank_cells(rank_groups(cell_weights)));
+    return best_cell_group[Math.floor(Math.random() * best_cell_group.length)];
+}
+
+function best_cells(cell_weights) {
+    var best_cell_group = [];
+    var values = [];
+    for (var i in cell_weights) {
+        if (typeof cell_weights[i] !== 'function') {
+            values.push(cell_weights[i]);
+        }
+    }
+    var max = Array.max(values);
+    var min = Array.min(values);
+    if (Math.abs(max) > Math.abs(min)) {
+        for (var i in cell_weights) {
+            if (typeof cell_weights[i] !== 'function') {
+                if (cell_weights[i] >= max) {
+                    best_cell_group.push(i);
+                }
+            }
+        }
+    } else {
+        for (var i in cell_weights) {
+            if (typeof cell_weights[i] !== 'function') {
+                if (cell_weights[i] <= min) {
+                    best_cell_group.push(i);
+                }
+            }
+        }
+    }
+    /*if(user_interface.ai_player == 0) {
+     for (var i in cell_weights) {
+     if (typeof cell_weights[i] !== 'function') {
+     if(cell_weights[i] >= max) {
+     best_cell_group.push(i);
+     }
+     }
+     }
+     } else if (user_interface.ai_player == 1) {
+     for (var i in cell_weights) {
+     if (typeof cell_weights[i] !== 'function') {
+     if(cell_weights[i] <= min) {
+     best_cell_group.push(i);
+     }
+     }
+     }
+     }*/
+    return best_cell_group;
+}
+
+function rank_groups(cell_weights) {
+    for (var i in cell_weights) {
+        if (typeof cell_weights[i] !== 'function') {
+            for (var j = 0; j < cell_weights[i].length; j++) {
+                var current_rank = cell_weights[i][j];
+                cell_weights[i][j] = current_rank[0] - current_rank[1];
+            }
+        }
+    }
+    return cell_weights;
+}
+
+function cell_group_rank(cell_group) {
+    var count_x = 0;
+    var count_o = 0;
+    for (var i = 0; i < cell_group.length; i++) {
+        if (user_interface.cells[cell_group[i]].innerHTML == items[0]) {
+            count_x++;
+        } else if (user_interface.cells[cell_group[i]].innerHTML == items[1]) {
+            count_o++;
+        }
+    }
+    return [count_x, count_o];
+}
+
+function rank_cells(cell_weights) {
+    for (var i in cell_weights) {
+        if (typeof cell_weights[i] !== 'function') {
+            var max = -(user_interface.grid_size);
+            for (var j = 0; j < cell_weights[i].length; j++) {
+                if (cell_weights[i][j] > max) {
+                    max = cell_weights[i][j];
+                }
+            }
+            cell_weights[i] = max;
+        }
+    }
+    return cell_weights;
 }
 
 function check(cell) {
-    var cell_groups = [get_row_group((Math.floor(cell / 3) + 1)), get_column_group((cell % 3) + 1)];
+    return check_cell_groups(get_cell_groups(cell));
+}
+
+function get_cell_groups(cell) {
+    var cell_groups = [get_row_group((Math.floor(cell / user_interface.grid_size) + 1)), get_column_group((cell % user_interface.grid_size) + 1)];
     if (cell % 2 == 0) {
         if (cell % 4 == 0) {
             cell_groups.push([0, 4, 8]);
@@ -183,7 +287,7 @@ function check(cell) {
             cell_groups.push([2, 4, 6]);
         }
     }
-    return check_cell_groups(cell_groups);
+    return cell_groups;
 }
 
 function check_cell_groups(cell_groups) {
@@ -191,11 +295,12 @@ function check_cell_groups(cell_groups) {
     var group_count = 0;
     while (!line_complete && (group_count < cell_groups.length)) {
         line_complete = check_cells(cell_groups[group_count]);
-        if(line_complete) {
+        if (line_complete) {
             color(cell_groups[group_count]);
         }
         group_count++;
-    };
+    }
+    ;
     return line_complete;
 }
 
@@ -205,12 +310,12 @@ function check_cells(cells) {
     var count_o = 1;
     for (var i = 0; i < cells.length; i++) {
         if (user_interface.cells[cells[i]].innerHTML == items[0]) {
-            count_o++;
-        } else if (user_interface.cells[cells[i]].innerHTML == items[1]) {
             count_x++;
+        } else if (user_interface.cells[cells[i]].innerHTML == items[1]) {
+            count_o++;
         }
     }
-    if (count_o == 4 || count_x == 4) {
+    if (count_o > user_interface.grid_size || count_x > user_interface.grid_size) {
         isComplete = true;
     }
     return isComplete;
@@ -219,7 +324,7 @@ function check_cells(cells) {
 function get_row_group(row) {
     var row_cells = [];
     for (var i = 0; i < user_interface.cells.length; i++) {
-        if (((row - 1) * 3) <= i && i < (row * 3)) {
+        if (((row - 1) * user_interface.grid_size) <= i && i < (row * user_interface.grid_size)) {
             row_cells.push(i);
         }
     }
@@ -229,7 +334,7 @@ function get_row_group(row) {
 function get_column_group(column) {
     var column_cells = [];
     for (var i = 0; i < user_interface.cells.length; i++) {
-        if (i % 3 == (column - 1)) {
+        if (i % user_interface.grid_size == (column - 1)) {
             column_cells.push(i);
         }
     }
@@ -250,15 +355,23 @@ function debug_message(message) {
     }
 }
 
-function add_listener(elements, eventName, handler) {
+function add_listener(elements, event_name, handler) {
     for (var i = 0; i < elements.length; i++) {
-        x = elements[i];
-        if (x.addEventListener) {
-            x.addEventListener(eventName, handler, false);
-        } else if (x.attachEvent) {
-            x.attachEvent('on' + eventName, handler);
+        var e = elements[i];
+        if (e.addEventListener) {
+            e.addEventListener(event_name, handler, false);
+        } else if (e.attachEvent) {
+            e.attachEvent('on' + event_name, handler);
         } else {
-            x['on' + eventName] = handler;
+            e['on' + event_name] = handler;
         }
     }
 }
+
+Array.max = function(array) {
+    return Math.max.apply(Math, array);
+};
+
+Array.min = function(array) {
+    return Math.min.apply(Math, array);
+};
